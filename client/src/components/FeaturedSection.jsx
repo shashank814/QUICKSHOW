44c51d3514b0c7bf5cd7ac29ee4e727a514b1423
Shashank Singh <sshingh456@gmail.com>
2025-06-18 15:46:49 +0530

    Initial commit

---
+++ client/src/components/FeaturedSection.jsx
@@ -0,0 +1,39 @@
+
+import { ArrowRight } from 'lucide-react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import BlurCircle from './BlurCircle'
+import { dummyShowsData } from '../assets/assets'
+import MovieCard from './MovieCard'
+
+const FeaturedSection = () => {
+
+  const navigate = useNavigate()
+
+  return (
+    <div className="relative px-6 md:px-16 lg:px-36 py-12 text-white overflow-hidden">
+      <div className="flex items-center justify-between mb-10">
+        <BlurCircle top='0' right='-80px' />
+        <p className="text-2xl font-semibold">Now Showing</p>
+        <button 
+          onClick={() => navigate('/movies')} 
+          className="flex items-center gap-2 text-sm bg-primary hover:bg-primary-dull px-4 py-2 rounded-full cursor-pointer"
+        >
+          View All <ArrowRight className="w-4 h-4" />
+        </button>
+      </div>
+
+      <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
+        {dummyShowsData.slice(0, 4).map((show)=> (
+            <MovieCard key={show._id} movie={show}/>
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-20">
+        <button onClick={()=> {navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show more</button>
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedSection
