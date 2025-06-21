9a512f3b6da704f083107948361b2aba33f4a332
Shashank Singh <sshingh456@gmail.com>
2025-06-21 15:37:20 +0530

    feat: Add payment link to MyBooking component and implement booking cancellation logic in Inngest

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,6 +1,7 @@
 import Show from "../models/Show.js";
 import Booking from "../models/Bookings.js";
 import stripe from 'stripe'
+import { inngest } from "../inngest/index.js";
 
 
 // Function to check availability of selected seats for a movie
@@ -81,6 +82,15 @@ export const createBooking = async (req, res) => {
 
         booking.paymentLink = session.url
         await booking.save()
+
+        // Run inngest sheduler function to check payment status after 10 minutes 
+
+        await inngest.send({
+            name: "app/checkpayment",
+            data: {
+                bookingId: booking._id.toString()
+            }
+        })
 
         res.json({success: true, url: session.url})
 
