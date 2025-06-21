8df5f50c831f89ba5fe979fb28b1c99c336a4a05
Shashank Singh <sshingh456@gmail.com>
2025-06-21 16:16:16 +0530

    feat: Integrate Nodemailer for booking confirmation emails and update environment variables

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -1,5 +1,6 @@
 import stripe from 'stripe';
 import Booking from '../models/Bookings.js'
+import { inngest } from '../inngest/index.js';
 
 export const stripeWebhooks = async (request, response) => {
     const stripeInstance = new stripe(process.env.STRIPE_WEBHOOK_SECRET);
@@ -28,10 +29,16 @@ export const stripeWebhooks = async (request, response) => {
                     isPaid: true,
                     paymentLink: ""
                 })
-            }
+
+                // Send Confirmation Email
+                await inngest.send({
+                    name: "app/show.booked",
+                    data: {bookingId}
+                })
                 
                 break;
-        
+            }
+                        
             default:
                 console.log('Unhandled event type: ', event.type);
         }
