0e0f2ddd93f5f53e777ce501b5fd25ca5e744488
Shashank Singh <sshingh456@gmail.com>
2025-07-29 16:21:29 +0530

    fix: Use environment variable for server port configuration

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ import userRouter from './routes/userRoutes.js';
 import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 await connectDB()
 
